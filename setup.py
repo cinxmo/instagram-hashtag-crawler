73a4345ec4a71b7d12754c7871e99e2fbb6bab94
Cindy Mo <cindymo@Cindys-MacBook-Pro-2.local>
2019-03-30 19:53:55 -0700

    setup.py to install via pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='instagram-hashtag-crawler',
+    version='0.1dev',
+    packages=['instagram-hashtag-crawler',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
